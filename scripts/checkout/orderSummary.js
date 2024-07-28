20b6bb3c3ce905e7e7fa5a1265b794bc0a7d4d20
Abir Gupta <abir.gupta2005@gmail.com>
2024-07-28 18:04:37 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -8,10 +8,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
-
-// const today = dayjs();
-// const deliveryDate = today.add(7, 'days');
-// console.log(deliveryDate.format('dddd, MMMM  D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary () {
 	let cartSummaryHTML = "";
@@ -157,7 +154,9 @@ export function renderOrderSummary () {
 
 			// update the cart quantity on the page header
 			updateCartQuantity();
-
+      // regenerate HTML 
+      renderPaymentSummary();
+      
 			});
 		});
 
@@ -264,6 +263,7 @@ export function renderOrderSummary () {
 				const {productId, deliveryOptionId} = element.dataset;
 				updateDeliveryOption(productId, deliveryOptionId);
 				renderOrderSummary();
+        renderPaymentSummary();
 			})
 		});
 }
