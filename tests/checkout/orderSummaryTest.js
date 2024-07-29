5022af9c02c93e441d092c2ba0ff04835fde849e
Abir Gupta <abir.gupta2005@gmail.com>
2024-07-30 00:42:30 +0530

    working tests before removeFromCart function

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -32,6 +32,11 @@ describe('test suite: render order summary', () => {
 
     renderOrderSummary();
   });
+
+  afterEach(() => {
+    document.querySelector('.js-test-container')
+      .innerHTML = ``;
+  });
   
   
   it('displays the cart', () => {
@@ -46,9 +51,23 @@ describe('test suite: render order summary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
+    
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+    
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
 
-    document.querySelector('.js-test-container')
-      .innerHTML = ``;
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual('$10.90');
+
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
+  
   });
 
   it('removes a product', () => {
@@ -71,7 +90,13 @@ describe('test suite: render order summary', () => {
 
     expect(cart[0].productId).toEqual(productId2);
 
-    document.querySelector('.js-test-container')
-      .innerHTML = ``;
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
+  
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
+
   });
 });
--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -2,8 +2,14 @@ import {addToCart, cart, loadFromStorage} from '../../data/cart.js'
 
 
 describe('test suite: add to cart', () => {
-  it('adds an existing product to the cart', () => {
+
+  beforeEach(()=> {
     spyOn(localStorage, 'setItem')
+    
+  })
+
+
+  it('adds an existing product to the cart', () => {
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
@@ -17,13 +23,19 @@ describe('test suite: add to cart', () => {
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 1);
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', 
+      JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+        quantity: 2,
+        deliveryOPtionId: '1'
+      }])
+    );
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(2);
     
   });
 
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem')
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
@@ -34,7 +46,23 @@ describe('test suite: add to cart', () => {
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 1);
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
   });
 });
+
+
+
+// describe('test suite: remove from cart', () => {
+//   beforeEach(() => {
+//     spyOn(localStorage, 'setItem');
+//     spyOn(localStorage, 'getItem').and.callFake(() => {
+//       return JSON.stringify([{}]);
+//     });
+//   });
+// });
