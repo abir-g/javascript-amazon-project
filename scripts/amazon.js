8a2e16947746b859838869908df11c24e02bfab2
Abir Gupta <abir.gupta2005@gmail.com>
2024-07-31 23:46:01 +0530

    added event listener for enter key in the search bar

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -133,4 +133,12 @@ function renderProductsGrid() {
             const search = document.querySelector('.js-search-bar').value;
             window.location.href = `amazon.html?search=${search}`;
         });
+
+    document.querySelector('.js-search-bar')
+        .addEventListener('keydown', (event) => {
+            if(event.key === "Enter"){
+                const search = document.querySelector('.js-search-bar').value;
+                window.location.href = `amazon.html?search=${search}`;
+            }
+        });
 }
